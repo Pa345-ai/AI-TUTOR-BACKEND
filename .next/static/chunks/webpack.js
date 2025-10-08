ab6cca6273811f2d211990ecb4d077db8d4a1e1c
Pa345-ai <ruwanpuragepawannimeshranasing@gmail.com>
2025-10-08 08:58:34 +0000

    Remove Vite, switch fully to Next.js for Vercel

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ebaa44cccb72d0bf"; }
+/******/ 		__webpack_require__.h = function() { return "58cf48a698bacbaf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
